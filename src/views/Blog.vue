c7a8c83043aafaa37c03b8b3c20d61da2f68b4d0
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-13 10:27:59 -0600

    fetch Api

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,9 @@
 <template>
     <Titulo texto="Titulo de mi blog"/>
+    <button @click="ConsumirApi">comsumir APi</button>
+    <div v-for="item in arrayBlog" :key="item.id">
+        <p>{{item.id}}: {{ item.title }}</p>
+    </div>
 </template>
 
 <script>
@@ -8,6 +12,25 @@ import Titulo from "../components/Titulo"
 export default {
     components:{
         Titulo,
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async ConsumirApi() {
+            try {
+                const data= await fetch("https://jsonplaceholder.typicode.com/posts");
+                const array = await data.json();
+                this.arrayBlog=array;
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    created(){
+        this.ConsumirApi()
     }
 }
 </script>
