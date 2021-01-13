18dd4676428b1602b92956965fceb8946b17ee7a
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-13 09:48:09 -0600

    basics routers

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about">    
+    <TituloVue texto="Titulo de Abount"/>
   </div>
 </template>
+
+<script>
+import TituloVue from '../components/Titulo.vue'
+export default {
+  components:{
+    TituloVue
+  }
+}
+</script>
+
+<style>
+
+</style>
